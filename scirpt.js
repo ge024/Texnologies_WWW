3c09fe344887a74351fc24bb11a4d3299d277e3a
Giorgos <99767668+ge024@users.noreply.github.com>
2025-01-05 00:32:16 +0200

    Add files via upload

---
+++ scirpt.js
@@ -0,0 +1,8 @@
+function showHidden() {
+    var element = document.getElementsByClassName("section");
+    if (element.style.display === "none") {
+        element.style.display = "block";
+    } else {
+        element.style.display = "none";
+    }
+}
