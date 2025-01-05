09a1cdeaf19486605f459124c685ee0ddcc00f4b
Giorgos <99767668+ge024@users.noreply.github.com>
2025-01-05 18:12:56 +0200

    Add files via upload

--- scirpt.js
+++ scirpt.js
@@ -1,5 +1,19 @@
-function showSidebarSection(index) {
+function showSidebarSection(sidebarIndex) {
     const sections = document.querySelectorAll(".side-bar-section");
     sections.forEach((section) => (section.style.display = "none"));
-    sections[index].style.display = "block";
+    sections[sidebarIndex].style.display = "block";
+}
+
+function showBioSection(paragraphID) {
+    const paragraphs = document.querySelectorAll(".biography-main p");
+    paragraphs.forEach((paragraph) => {
+        paragraph.style.display = "none";
+    });
+
+    console.log(paragraphID);
+
+    const targerParagraph = document.getElementById(paragraphID);
+    if (targerParagraph) {
+        targerParagraph.style.display = "block";
+    }
 }
