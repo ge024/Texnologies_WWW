f6b560caea7eb1c87d4ed914f15f32c4c49b4af0
Giorgos <99767668+ge024@users.noreply.github.com>
2025-01-14 20:28:46 +0200

    Add files via upload

--- scirpt.js
+++ scirpt.js
@@ -1,19 +1,37 @@
 function showSidebarSection(sidebarIndex) {
-    const sections = document.querySelectorAll(".side-bar-section");
+    const sections = document.querySelectorAll(".sideBarSection");
     sections.forEach((section) => (section.style.display = "none"));
     sections[sidebarIndex].style.display = "block";
 }
-
+function showMainSection(sectionID) {
+    const sections = document.querySelectorAll(".mainSection");
+    sections.forEach((section) => {
+        section.style.display = "none";
+    });
+    const targetSection = document.getElementById(sectionID);
+    if (targetSection) {
+        targetSection.style.display = "block";
+    }
+}
 function showBioSection(paragraphID) {
-    const paragraphs = document.querySelectorAll(".biography-main p");
+    const paragraphs = document.querySelectorAll("#biographySection p");
     paragraphs.forEach((paragraph) => {
         paragraph.style.display = "none";
     });
-
-    console.log(paragraphID);
-
-    const targerParagraph = document.getElementById(paragraphID);
-    if (targerParagraph) {
-        targerParagraph.style.display = "block";
+    const targetParagraph = document.getElementById(paragraphID);
+    if (targetParagraph) {
+        targetParagraph.style.display = "block";
     }
 }
+
+fetch("data.json")
+    .then((response) => response.json())
+    .then((data) => {
+        data.biographySections.forEach((section) => {
+            const element = document.getElementById(section.id);
+            if (element) {
+                element.textContent = section.content;
+            }
+        });
+    })
+    .catch((error) => console.error("Error loading content", error));
