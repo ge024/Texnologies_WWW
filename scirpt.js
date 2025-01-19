4f1c0113228733ff578f69496ed20f96782c58b7
Giorgos <99767668+ge024@users.noreply.github.com>
2025-01-19 18:39:20 +0200

    Add files via upload

--- scirpt.js
+++ scirpt.js
@@ -1,3 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    fetchLinksData();
+    fetchGalleryData();
+});
+
+// Main and Sidebar
 function showSidebarSection(sidebarIndex) {
     const sections = document.querySelectorAll(".sideBarSection");
     sections.forEach((section) => (section.style.display = "none"));
@@ -13,16 +19,7 @@ function showMainSection(sectionID) {
         targetSection.style.display = "block";
     }
 }
-function showBioSection(paragraphID) {
-    const paragraphs = document.querySelectorAll("#biographySection p");
-    paragraphs.forEach((paragraph) => {
-        paragraph.style.display = "none";
-    });
-    const targetParagraph = document.getElementById(paragraphID);
-    if (targetParagraph) {
-        targetParagraph.style.display = "block";
-    }
-}
+
 function showPaintingSection(containerID) {
     const containers = document.querySelectorAll(".hiddenContainer");
     containers.forEach((container) => container.classList.remove("paintingsContainer"));
@@ -32,7 +29,8 @@ function showPaintingSection(containerID) {
     }
 }
 
-fetch("data.json")
+// Biograhphy Section
+fetch("data/biography.json")
     .then((response) => response.json())
     .then((data) => {
         data.biographySections.forEach((section) => {
@@ -44,4 +42,94 @@ fetch("data.json")
     })
     .catch((error) => console.error("Error loading content", error));
 
-document.getElementById();
+function showBioSection(paragraphID) {
+    const paragraphs = document.querySelectorAll("#biographySection p");
+    paragraphs.forEach((paragraph) => {
+        paragraph.style.display = "none";
+    });
+    const targetParagraph = document.getElementById(paragraphID);
+    if (targetParagraph) {
+        targetParagraph.style.display = "block";
+    }
+}
+// Galleries
+function fetchGalleryData() {
+    fetch("data/galleries.json")
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then((data) => {
+            loadGalleries(data.galleries);
+        })
+        .catch((error) => {
+            console.error("Error", error);
+        });
+}
+function loadGalleries(galleries) {
+    const galsTableBody = document.getElementById("galsTableBody");
+    galsTableBody.innerHTML = "";
+
+    galleries.forEach((gallery) => {
+        const row = document.createElement("tr");
+        let linkHtml = `<a href="${gallery.url}" target="_blank">Click me</a>`;
+
+        row.innerHTML = `
+            <td>${gallery.title}</td>
+            <td>${linkHtml}</td>
+        `;
+        galsTableBody.appendChild(row);
+    });
+}
+function showGalleriesSection(sectionID) {
+    const sections = document.querySelectorAll(".hidden");
+    sections.forEach((section) => {
+        section.style.display = "none";
+    });
+    const targetSection = document.getElementById(sectionID);
+    if (targetSection) {
+        targetSection.style.display = "block";
+    }
+}
+
+// Links
+function fetchLinksData() {
+    fetch("data/links.json")
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then((data) => {
+            loadLinks(data.links);
+        })
+        .catch((error) => {
+            console.error("Error", error);
+        });
+}
+function loadLinks(links) {
+    const linksTableBody = document.getElementById("linksTableBody");
+    linksTableBody.innerHTML = "";
+    links.forEach((link) => {
+        const row = document.createElement("tr");
+        const linkHtml = `<a href="${link.url}" target="_blank">Click me</a>`;
+        row.innerHTML = `
+        <td>${link.title}</td>
+        <td>${linkHtml}</td>
+      `;
+        linksTableBody.appendChild(row);
+    });
+}
+function showLinksSection(sectionID) {
+    const sections = document.querySelectorAll(".hidden");
+    sections.forEach((section) => {
+        section.style.display = "none";
+    });
+    const targetSection = document.getElementById(sectionID);
+    if (targetSection) {
+        targetSection.style.display = "block";
+    }
+}
